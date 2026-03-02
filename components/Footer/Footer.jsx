9d7c62cee99555a609bc80cd77e6824c32a9feb4
Michelle Souliotis <michelle.souliotis@gmail.com>
2026-03-03 01:30:43 +0530

    Change the Footer component to use a <footer> tag instead of a React fragment.

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import LinkedInLogo from "@/assets/icons/linkedin.svg";
 
 export default function Footer() {
     return (
-        <>
+        <footer>
             <div className="flex flex-col gap-5 mx-4 py-5.5 md:mx-10 md:py-5 md:flex-row-reverse md:justify-between md:items-center lg:mx-26 lg:py-7">
                 <div className="flex md:gap-10">
                     <div className="flex flex-col gap-4 font-roboto text-lg text-[#1A1B22] md:flex-row">
@@ -25,6 +25,6 @@ export default function Footer() {
                 </div>
                 <div className="font-roboto text-base text-[#B6BCBF] text-center ">© 2026 Supersite, Powered by News API</div>
             </div>
-        </>
+        </footer>
     )
 }
